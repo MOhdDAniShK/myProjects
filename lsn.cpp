09e7a9f588da27ebb26deea84f631e99a0fb7c4d
MOhdDAniShK <dmohd1939@gmail.com>
2025-10-13 01:55:32 +0530

    add lsn.cpp

---
+++ lsn.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+
+int main(){
+    int inp,round,n=1;
+    cout<<"enter no. of round you want play:";
+    cin>>round;
+
+    while (n<=round)
+    {
+        // Taking User Input.
+
+        cout<<"Select:\n--> 0 FOR Stone\n--> 1 FOR Paper\n--> 2 FOR Scissor\nWhat's your choice: "<<endl;
+        cin>>inp;
+
+        //Checking For the validity of the Input.
+
+        if(inp > 2){
+            cout<<"Invalid Input.";
+            return 0;
+        }
+
+        //selecting the outcome.
+
+        int ran = (rand()%3);
+        cout<<"Computer's choice is: "<<ran<<"\n\n";
+        string mat[3][3]= {{"DRAW","LOST","WON" },
+                           {"WON" ,"DRAW","LOST"},
+                           {"LOST","WON" ,"DRAW"}};
+
+        // Printing the outcome.
+
+        cout<<"-------- YOU "<< mat[inp][ran]<<" THIS ROUNDs --------"<<"\n\n";
+        n +=1;
+    }
+    
+}
